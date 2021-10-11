850cd571e3f90828238483830585d694fc138d6b
Aleksei Naumyshev <naumyshev@yandex.ru>
2021-10-11 07:29:11 +0300

    add isPalindrome

--- js/Define a card suit.js
+++ js/Define a card suit.js
@@ -1,12 +1,12 @@
 function defineSuit(card) {
-    
-    if(card[1]=='♣'){
-      return 'clubs';
-    } else if(card[1]=='♦'){
-      return 'diamonds';
-    } else if(card[1]=='♥'){
-      return 'hearts';
-    } else {
-      return 'spades';
-    }
+  // good luck
+  if(card[card.length-1] =='♣'){
+    return 'clubs';
+  } else if(card[card.length-1] =='♦'){
+    return 'diamonds';
+  } else if(card[card.length-1] =='♥'){
+    return 'hearts';
+  } else {
+    return 'spades';
   }
+}
