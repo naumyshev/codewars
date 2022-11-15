f9f071b6e53d20498194f34987dec4b133812501
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-11-15 15:21:00 +0300

    Completed 8 kyu Grasshopper - Basic Function Fixer

--- test.js
+++ test.js
@@ -2,3 +2,7 @@
 // 8 kyu
 // How many lightsabers do you own?
 const howManyLightsabersDoYouOwn = name => name === "Zach" ? 18 : 0
+
+// 8 kyu
+// Grasshopper - Basic Function Fixer
+const addFive = num => num + 5
