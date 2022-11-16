0d4365c59bbef0b4d3d8794f71c91300ced1c853
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-11-16 16:57:17 +0300

    Completed 8 kyu Will you make it?

--- test.js
+++ test.js
@@ -6,3 +6,7 @@ const howManyLightsabersDoYouOwn = name => name === "Zach" ? 18 : 0
 // 8 kyu
 // Grasshopper - Basic Function Fixer
 const addFive = num => num + 5
+
+// 8 kyu
+// Will you make it?
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump
