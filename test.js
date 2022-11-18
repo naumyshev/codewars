56564427d2ea19794279b25a79ff59981576f96a
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-11-18 09:43:18 +0300

    Completed 8 kyu Keep up the hoop

--- test.js
+++ test.js
@@ -14,3 +14,7 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceTo
 // 8 kyu
 // String Templates - Bug Fixing #5
 const buildString = (...template) => `I like ${template.join(', ')}!`
+
+// 8 kyu
+// Keep up the hoop
+const hoopCount = n => n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
