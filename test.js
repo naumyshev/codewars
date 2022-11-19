8bd6fe0d12de943e0372ddc0a8366822e438a58d
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-11-19 10:07:23 +0300

    Completed 8 kyu Keep up the hoop

--- test.js
+++ test.js
@@ -18,3 +18,7 @@ const buildString = (...template) => `I like ${template.join(', ')}!`
 // 8 kyu
 // Keep up the hoop
 const hoopCount = n => n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+
+// 8 kyu
+// Invert values
+const invert = (array) => array.map(el => el * -1)
